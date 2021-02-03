048af42818e9e021c11ddd94a4e18a81ed14eea1
JosePorras <joseporras291@gmail.com>
2021-02-03 09:38:12 -0500

    Changes have been made, especially the DB

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,14 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
-from django.views.generic.edit import CreateView
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 #Here we create the template for the home view.
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
+   # template_name = 'login.html'
 
 
 class BlogDetailView(DetailView):
@@ -20,3 +21,15 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = '__all__'    
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']     
+
+
+class BlogDeleteView(DeleteView):
+     model = Post
+     template_name = 'post_delete.html'
+     success_url = reverse_lazy('home')
+
