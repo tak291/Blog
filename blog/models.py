2c3b6dbc18a426a365ff97bb6695d2eb28450d94
jose <joseporras291@gmail.com>
2021-02-11 01:19:15 -0500

    Working CRUD now need to finish with the styling and the text message

--- blog/models.py
+++ blog/models.py
@@ -7,33 +7,22 @@ from phone_field import PhoneField
 #Job model.
 class Post(models.Model):
     #To receiver the title and worker
-    # . Author has a primary key
+    # . Worker has a primary key
     job = models.CharField(max_length=200)
     worker = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     ) 
 
-    #Intake the body of the blog
     details = models.TextField()
-    customer_name = models.CharField(max_length=200, default="Cutomer name")
+    customer_name = models.CharField(max_length=200, default=" ")
     Employee_phone_number = PhoneField(blank=True, help_text='Employee phone.')        
     customer_number = PhoneField(blank=True, help_text='Customer phone')    
+    addres = models.CharField(max_length=200, default="")
+    city = models.CharField(max_length=200, default="")
 
 
-    #How to return the title of the blog to the users.
-    def __str__(self):
-        return self.job
-
-
-    #Need to verify what this does exactly.
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[str(self.id)])
-
-    def __str__(self):
-        return self.job
-
-
+    #Status of the job.
     OPEN = 1
     PENDING_APPOINTMENT =  2 
     CLOSED = 3
@@ -53,4 +42,18 @@ class Post(models.Model):
             return ("Pending")
         else: 
             return("Closed")        
-        
+ 
+    #How to return the title of the blog to the users.
+    def __str__(self):
+        return self.job
+
+
+    #Return the id.
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.id)])
+
+
+
+       
+
+
